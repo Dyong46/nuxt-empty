67123034d69e42cf7b508b79587495b6d2f898dd
Dylan Vu <96007994+Dyong46@users.noreply.github.com>
2024-09-06 18:20:21 +0700

    step 1

--- components/Booking/ChooseTime.vue
+++ components/Booking/ChooseTime.vue
@@ -4,10 +4,28 @@
       <v-card-title class="text-h5 font-weight-bold">
 				Lake {{ lake.name }} - Dock {{ lake.dock }}
 			</v-card-title>
-      <div v-for="(boat, subIndex) in lake.boat" :key="`${index}-${subIndex}`">
-				<span class="font-weight-bold">Boat</span> {{ boat.name }}
-				<v-icon icon="mdi-account-outline"/>
-				
+			<div>
+				<div class="ps-4 mb-4" v-for="(boat, subIndex) in lake.boat" :key="`${index}-${subIndex}`">
+					<v-layout class="mb-3">
+						<div style="width: 250px;"><span class="font-weight-bold">Boat</span> {{ boat.name }}</div>
+						<div style="width: 250px;"><v-icon class="font-weight-bold" icon="mdi-account-outline"/> 40</div>
+						<div><span class="font-weight-bold">HP</span> 100</div>
+					</v-layout>
+					
+					<div class="d-flex flex-nowrap overflow-x-auto">
+						<v-btn 
+							v-for="n in 7" 
+							:key="`${index}-${subIndex}-${n}`"
+							prepend-icon="mdi-calendar-check-outline"
+							class="ms-2" 
+							variant="outlined"
+							@click="handleClick(boat.name, n)"
+							:class="color(boat.name, n)"
+						>
+							{{ n }}/9/2024
+						</v-btn>
+					</div>
+				</div>
 			</div>
     </v-card>
 	</div>
@@ -43,7 +61,16 @@ export default {
 						}
 					]
 				}
-			]
+			],
+			selectedDate: null,
+		}
+	},
+	methods: {
+		handleClick(boatName, day) {
+			this.selectedDate = `${boatName}.${day}`;
+		},
+		color(boatName, day) {
+			return this.selectedDate === `${boatName}.${day}` ? "bg-blue" : "";
 		}
 	}
 };
